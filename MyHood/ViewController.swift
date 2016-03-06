889ba50d7606b012a27ae273b461381275b6fea6
Rea Won Kim <ngoccuong11789@gmail.com>
2016-03-06 21:26:41 +0700

    no message

---
+++ MyHood/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  MyHood
+//
+//  Created by Rea Won Kim on 3/3/16.
+//  Copyright © 2016 dammelaptrinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        DataService.instance.loadedPosts
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "onPostsLoaded:", name: "postsLoaded", object: nil)
+        
+    }
+
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let post = DataService.instance.loadedPosts[indexPath.row]
+        if let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as? PostCell {
+            cell.configureCell(post)
+            return cell
+        }else {
+            let cell = PostCell()
+            cell.configureCell(post)
+            return cell
+        }
+        
+        
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 85
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataService.instance.loadedPosts.count
+    }
+    func onPostsLoaded(notif: AnyObject) {
+        tableView.reloadData()
+    }
+    
+    
+}
+
